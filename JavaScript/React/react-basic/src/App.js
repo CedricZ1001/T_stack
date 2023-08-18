495946a01f94f5e42b3f09f9d18e3873be5ccc0a
CedricZ1001 <Cedric1001Z@gmail.com>
2023-08-18 17:50:01 +0800

    update react

--- JavaScript/React/react-basic/src/App.js
+++ JavaScript/React/react-basic/src/App.js
@@ -1,33 +1,42 @@
-import React, { createRef } from "react"
-import "./App.css"
+import React from 'react'
 
-class Counter extends React.Component {
-  msgRef = createRef()
+// 函数式子组件
+function FSon (props) {
+  console.log(props)
+  return (
+    <div>
+      子组件1
+      {props.msg}
+    </div>
+  )
+}
 
-
-  getValue () {
-    console.log(this.msgRef.current.value)
-  }
-
+// 类子组件
+class CSon extends React.Component {
   render () {
     return (
-      <>
-        <input
-          type="text"
-          ref={this.msgRef}
-        />
-        <button onClick={() => this.getValue()}>click</button>
-      </>
+      <div>
+        子组件2
+        {this.props.msg}
+      </div>
     )
   }
 }
-
-function App () {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  )
+// 父组件
+class App extends React.Component {
+  state = {
+    message: 'this is message'
+  }
+  render () {
+    return (
+      <div>
+        <div>父组件</div>
+        <FSon msg={this.state.message} />
+        <CSon msg={this.state.message} />
+        
+      </div>
+    )
+  }
 }
 
 export default App
